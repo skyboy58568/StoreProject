fbcac6a85fc93b52b9bc34e9e14f94c2e15f9ca5
skyboy58568 <chisreerag@gmail.com>
2024-02-08 11:46:55 +0530

    second  commit

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const connectDB = require("./db/connect");
 const productRouter = require("./routes/products");
 const notfoundError = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
+const dotenv = require('dotenv')
+
+dotenv.config({path:'./config.env'})
+
 
 
 
 const app = express();
 
 connectDB(
-    "mongodb://chisreerag:1234@ac-4hh06rk-shard-00-01.ivxs4r0.mongodb.net:27017,ac-4hh06rk-shard-00-02.ivxs4r0.mongodb.net:27017,ac-4hh06rk-shard-00-00.ivxs4r0.mongodb.net:27017/StoreAPI?authSource=admin&replicaSet=atlas-7k1zb8-shard-0&retryWrites=true&w=majority&tls=true"
+   process.env.MONGO_URL
   )
     .then(() => console.log("Mongo server started"))
     .catch((err) => console.log("Mongo Error"));
